386a04fc76bf057517aaa5f903e4dc92a9d5bddf
nainghtetlinn <naing.work.it@gmail.com>
2024-04-28 13:27:11 +0630

    refactor: extract DarkmodeBtn component

--- src/app/(customer)/_components/Nav.tsx
+++ src/app/(customer)/_components/Nav.tsx
@@ -1,19 +1,20 @@
 "use client"
 
-import { Menu, Ticket, Headset, ShoppingCart, Moon, Sun } from "lucide-react"
+import DarkModeBtn from "@/components/DarkModeBtn"
 import { Button } from "@/components/ui/button"
 import {
   DropdownMenu,
-  DropdownMenuTrigger,
-  DropdownMenuPortal,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { Headset, Menu, ShoppingCart, Ticket } from "lucide-react"
+
+import { useScrollDirection } from "@/hooks/useScrollDirection"
 import clsx from "clsx"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
-import { useTheme } from "next-themes"
-import { useScrollDirection } from "@/hooks/useScrollDirection"
 
 const Nav = () => {
   const path = usePathname()
@@ -35,36 +36,6 @@ const Nav = () => {
           </div>
         </div>
       </nav>
-    </>
-  )
-}
-
-const themeModes = ["light", "dark", "system"]
-const DarkModeBtn = () => {
-  const { setTheme, theme } = useTheme()
-
-  return (
-    <>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="outline" size="icon">
-            <Sun className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <Moon className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuPortal>
-          <DropdownMenuContent side="bottom" align="end">
-            {themeModes.map((mode) => (
-              <DropdownMenuItem
-                key={mode}
-                className={clsx("capitalize", mode == theme && "bg-accent")}
-                onClick={() => setTheme(mode)}>
-                {mode}
-              </DropdownMenuItem>
-            ))}
-          </DropdownMenuContent>
-        </DropdownMenuPortal>
-      </DropdownMenu>
     </>
   )
 }
---
+++ src/components/DarkModeBtn.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+import clsx from "clsx"
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+import { Button } from "./ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu"
+
+const themeModes = ["light", "dark", "system"]
+
+const DarkModeBtn = () => {
+  const { setTheme, theme } = useTheme()
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" size="icon">
+            <Sun className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuPortal>
+          <DropdownMenuContent side="bottom" align="end">
+            {themeModes.map((mode) => (
+              <DropdownMenuItem
+                key={mode}
+                className={clsx("capitalize", mode == theme && "bg-accent")}
+                onClick={() => setTheme(mode)}>
+                {mode}
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenuPortal>
+      </DropdownMenu>
+    </>
+  )
+}
+
+export default DarkModeBtn
