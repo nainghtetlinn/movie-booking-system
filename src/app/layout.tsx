2e90eeb01aac7c340fc7d6fb4ba77ff6f8392fa8
nainghtetlinn <naing.work.it@gmail.com>
2024-05-01 21:43:31 +0630

    fix: typo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 
-import SessionProvider from "@/providers/server-provider"
+import SessionProvider from "@/providers/session-provider"
 import ThemeProvider from "@/providers/theme-provider"
 
 const inter = Inter({ subsets: ["latin"] })
