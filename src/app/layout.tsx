531cebbad4adb1554088d1828a74f7c64e5bb4f0
nainghtetlinn <naing.work.it@gmail.com>
2024-05-02 20:58:55 +0630

    install: sonner component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css"
 
 import SessionProvider from "@/providers/session-provider"
 import ThemeProvider from "@/providers/theme-provider"
+import { Toaster } from "@/components/ui/sonner"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -26,6 +27,7 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange>
             <main>{children}</main>
+            <Toaster />
           </ThemeProvider>
         </SessionProvider>
       </body>
