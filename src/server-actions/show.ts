1349149ce4cdcf382a52ff76ef6399c8342012ca
nainghtetlinn <naing.work.it@gmail.com>
2024-05-27 17:55:59 +0630

    fix: create seats for show

--- src/server-actions/show.ts
+++ src/server-actions/show.ts
@@ -1,5 +1,6 @@
 "use server"
 
+import { Prisma } from "@prisma/client"
 import { TShow } from "@/types/show"
 import { showSchema } from "@/validators/show"
 import { Show } from "@prisma/client"
@@ -12,7 +13,15 @@ export async function createShow(data: TShow): Promise<TReturn> {
   try {
     const validation = showSchema.safeParse(data)
     if (!validation.success) return { success: false, message: "Invalid inputs." }
+    // create show
     const show = await db.show.create({ data })
+
+    // create show's seats
+    const showSeatRelations: Prisma.ShowSeatRelationCreateManyInput[] = []
+    const seats = await db.seat.findMany()
+    seats.forEach((seat) => showSeatRelations.push({ seatId: seat.id, showId: show.id }))
+    await db.showSeatRelation.createMany({ data: showSeatRelations })
+
     revalidatePath("/dashboard/shows")
     return { success: true, data: show }
   } catch (error) {
@@ -34,7 +43,12 @@ export async function editShow(id: string, data: TShow): Promise<TReturn> {
 
 export async function deleteShow(id: string): Promise<TReturn> {
   try {
+    // delete show
     const show = await db.show.delete({ where: { id } })
+
+    // delete show's seats
+    await db.showSeatRelation.deleteMany({ where: { showId: show.id } })
+
     revalidatePath("/dashboard/shows")
     return { success: true, data: show }
   } catch (error) {
