9a9086fe6b9512a3fe5f1beae02b52c2d74be644
nainghtetlinn <naing.work.it@gmail.com>
2024-06-05 18:48:07 +0630

    fix: dashboard grid for improvement

--- src/app/(admin)/dashboard/_components/DashboardGrid.tsx
+++ src/app/(admin)/dashboard/_components/DashboardGrid.tsx
@@ -8,12 +8,12 @@ import { useQuery } from "@tanstack/react-query"
 import { fetchDashboardData } from "@/server-actions/dashboard"
 
 const DashboardGrid = ({ range }: { range: string }) => {
-  const { data, isLoading, isSuccess } = useQuery({
+  const { data, isFetching, isSuccess } = useQuery({
     queryKey: ["dashboard", range],
     queryFn: () => fetchDashboardData(range),
   })
 
-  if (isLoading)
+  if (isFetching)
     return (
       <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
         <Skeleton className="h-[150px] rounded-lg" />
--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,8 +1,6 @@
 import DashboardGrid from "./_components/DashboardGrid"
 import Filter from "@/components/Filter"
 
-import { HydrationBoundary, QueryClient, dehydrate } from "@tanstack/react-query"
-import { fetchDashboardData } from "@/server-actions/dashboard"
 import { redirect } from "next/navigation"
 
 const Dashboard = async ({
@@ -10,21 +8,8 @@ const Dashboard = async ({
 }: {
   searchParams: { range?: "day" | "week" | "month" }
 }) => {
-  const qc = new QueryClient()
-
-  await qc.prefetchQuery({
-    queryKey: ["dashboard", "day"],
-    queryFn: () => fetchDashboardData("day"),
-  })
-
   if (searchParams.range && !["day", "week", "month"].includes(searchParams.range))
     redirect("/dashboard")
-
-  if (searchParams.range)
-    await qc.prefetchQuery({
-      queryKey: ["dashboard", searchParams.range],
-      queryFn: () => fetchDashboardData(searchParams.range!),
-    })
 
   return (
     <>
@@ -33,9 +18,7 @@ const Dashboard = async ({
           <h2 className="text-3xl font-bold">Dashboard</h2>
           <Filter searchParamKey="range" defaultOptions="day" options={["day", "week", "month"]} />
         </div>
-        <HydrationBoundary state={dehydrate(qc)}>
-          <DashboardGrid range={searchParams.range || "day"} />
-        </HydrationBoundary>
+        <DashboardGrid range={searchParams.range || "day"} />
       </section>
     </>
   )
